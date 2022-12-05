5ce8c4656d145b38f7e4cef324cf1f2c3c38ae2a
Sergiy B <bisecom@gmail.com>
2022-12-05 22:27:46 +0200

    initial commit

---
+++ src/test/java/com/itemis/coding/challenge/taxes/TaxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itemis.coding.challenge.taxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
