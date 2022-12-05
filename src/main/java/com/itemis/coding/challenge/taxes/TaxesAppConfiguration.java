ad51487f096b2452dd10aeb1f1c451884a858e2e
Sergiy B <bisecom@gmail.com>
2022-12-05 23:41:43 +0200

    getAllItems should return list of initiated items

--- src/main/java/com/itemis/coding/challenge/taxes/TaxesAppConfiguration.java
+++ src/main/java/com/itemis/coding/challenge/taxes/TaxesAppConfiguration.java
@@ -1,9 +1,13 @@
 package com.itemis.coding.challenge.taxes;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.itemis.coding.challenge.taxes.model.Item;
+import com.itemis.coding.challenge.taxes.model.Origin;
+import com.itemis.coding.challenge.taxes.model.PreferentialTax;
 import com.itemis.coding.challenge.taxes.repository.ItemRepositoryImpl;
 
 @Configuration
@@ -16,6 +20,9 @@ public class TaxesAppConfiguration {
 
   @Bean
   public ItemRepositoryImpl itemRepository() {
-    return new ItemRepositoryImpl();
+    Item book = new Item(1, "book", 12.49, Origin.DOMESTIC, PreferentialTax.BOOK);
+    Item musicCd = new Item(2, "music CD", 14.99, Origin.DOMESTIC, PreferentialTax.OTHER);
+    Item chocolateBar = new Item(3, "chocolate bar", .85, Origin.DOMESTIC, PreferentialTax.FOOD);
+    return new ItemRepositoryImpl(List.of(book, musicCd, chocolateBar));
   }
 }
--- src/test/java/com/itemis/coding/challenge/taxes/repository/ItemRepositoryImplTest.java
+++ src/test/java/com/itemis/coding/challenge/taxes/repository/ItemRepositoryImplTest.java
@@ -1,21 +1,44 @@
 package com.itemis.coding.challenge.taxes.repository;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import com.itemis.coding.challenge.taxes.model.Item;
+import com.itemis.coding.challenge.taxes.model.Origin;
+import com.itemis.coding.challenge.taxes.model.PreferentialTax;
+
 public class ItemRepositoryImplTest {
   private ItemRepository itemRepository;
+  private Item book;
+  private Item musicCd;
+  private Item chocolateBar;
 
   @BeforeEach
   public void initEach() {
-    itemRepository = new ItemRepositoryImpl(new ArrayList<>());
+    book = new Item(1, "book", 12.49, Origin.DOMESTIC, PreferentialTax.BOOK);
+    musicCd = new Item(2, "music CD", 14.99, Origin.DOMESTIC, PreferentialTax.OTHER);
+    chocolateBar = new Item(3, "chocolate bar", .85, Origin.DOMESTIC, PreferentialTax.FOOD);
+    itemRepository = new ItemRepositoryImpl(List.of(book, musicCd, chocolateBar));
   }
 
   @Test
   public void whenCallGetItemListShouldReturnNotNull() {
     Assertions.assertNotNull(itemRepository.getAllItems());
+  }
+
+  @Test
+  public void whenCallGetItemListShouldReturnItemsList() {
+    List<Item> items = itemRepository.getAllItems();
+
+    Assertions.assertEquals(3, items.size());
+    Assertions.assertEquals(book.getName(), items.get(0).getName());
+    Assertions.assertEquals(book.getNetPrice(), items.get(0).getNetPrice());
+    Assertions.assertSame(book.getOrigin(), items.get(0).getOrigin());
+    Assertions.assertSame(book.getPreferentialTax(), items.get(0).getPreferentialTax());
+    Assertions.assertEquals(musicCd.getName(), items.get(1).getName());
+    Assertions.assertEquals(chocolateBar.getName(), items.get(2).getName());
   }
 }
