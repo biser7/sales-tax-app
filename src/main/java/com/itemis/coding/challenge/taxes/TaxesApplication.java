d19dbdb5f6a57815ff5f0980e2785d3c0996edf3
Sergiy B <bisecom@gmail.com>
2022-12-06 14:02:21 +0200

    implement CommandLineRunner

--- src/main/java/com/itemis/coding/challenge/taxes/TaxesApplication.java
+++ src/main/java/com/itemis/coding/challenge/taxes/TaxesApplication.java
@@ -1,13 +1,28 @@
 package com.itemis.coding.challenge.taxes;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaxesApplication {
+public class TaxesApplication implements CommandLineRunner {
+	private static final Logger LOGGER = LoggerFactory.getLogger(TaxesApplication.class);
+	private final DemoReceipts demoReceipts;
+
+	public TaxesApplication(DemoReceipts demoReceipts) {
+		this.demoReceipts = demoReceipts;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaxesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		LOGGER.info("Application is started!");
+		demoReceipts.runApp();
+		LOGGER.info("Application is closed!");
+	}
 }
