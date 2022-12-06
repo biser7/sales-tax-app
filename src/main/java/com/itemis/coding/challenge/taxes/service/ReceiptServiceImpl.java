1f1c3e671ae69d7a15a3c7dcda0a4e8a80960570
Sergiy B <bisecom@gmail.com>
2022-12-06 13:03:33 +0200

    implement printReceipt method

--- src/main/java/com/itemis/coding/challenge/taxes/service/ReceiptServiceImpl.java
+++ src/main/java/com/itemis/coding/challenge/taxes/service/ReceiptServiceImpl.java
@@ -11,6 +11,13 @@ public class ReceiptServiceImpl implements ReceiptService {
 
   @Override
   public void printReceipt() {
-
+    System.out.println("Client Receipt:");
+    receipt
+      .getShopCart()
+      .getItemToQuantity()
+      .forEach((item, quantity) -> System.out.printf("> %d %s: %.2f\n", quantity, item.getName(),
+        receipt.getItemAmount(item)));
+    System.out.printf("> Sales Taxes: %.2f\n", receipt.getSaleTax());
+    System.out.printf("> Total: %.2f\n", receipt.getTotalAmount());
   }
 }
